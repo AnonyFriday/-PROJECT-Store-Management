6ddd2320e1ca3f6bc81bea9de251bf70dd22625c
AnonyFriday <duyvukim752000@gmail.com>
2023-09-22 00:13:03 +0700

    Cleaning redundant spaces

--- src/tools/Utilities.java
+++ src/tools/Utilities.java
@@ -1,10 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tools;
 
-import java.lang.System.Logger;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;

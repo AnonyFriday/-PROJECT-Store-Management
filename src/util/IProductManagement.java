db419902f411339845a69c5080cf5d08ac144a53
duyvukim <56812770+AnonyFriday@users.noreply.github.com>
2023-09-23 21:45:25 +0700

    Adding ProductManagement Interface

---
+++ src/util/IProductManagement.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import purchasingmng.entities.Product;
+
+/** A common interface contains related actions for handling products
+ *
+ * @author duyvu
+ */
+public interface IProductManagement {
+
+    // ======================================
+    // = ADD GROUPS
+    // ======================================
+    public abstract boolean addProducts(int noProducts);
+
+    // ======================================
+    // = UPDATE GROUPS
+    // ======================================
+    public abstract boolean updateProduct(String pID);
+
+    public abstract void sortProducts(Comparator<Product> comparator);
+
+    // ======================================
+    // = DISPLAYGROUPS
+    // ======================================
+    public abstract boolean displayProducts();
+
+    public abstract boolean displayProducts(Function<Product, Boolean> verify);
+}
